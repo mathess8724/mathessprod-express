5feee00839ecf01ce04cc9cd35c1f93d9b747bed
Mathessprod <mathesmsm@gmail.com>
2021-02-07 09:30:20 +0000

    ports

--- app.js
+++ app.js
@@ -45,7 +45,12 @@ app.route('/api/captcha')
     res.send('Update the book')
   })
 
-  app.listen(process.env.PORT || 8080, function() {
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+ /*  app.listen(process.env.PORT || 8080, function() {
     console.log('Server is listenning on port'+ process.env.PORT );
-})
+}) */
 
