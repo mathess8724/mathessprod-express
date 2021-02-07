b0f124b287e80bb446033f0f961b468701ab7610
Mathessprod <mathesmsm@gmail.com>
2021-02-07 10:14:44 +0000

    s

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ app.route('/api/captcha')
 //const port = argv.port || 8081;
 var port = process.env.PORT || 3002
 
-app.listen(port, '0.0.0.0', ()=>{
-    console.log('Probably listening to heroku $PORT now ', port); // unless $PORT is undefined, in which case you're listening to 8081.
+app.listen(process.env.PORT, '0.0.0.0', ()=>{
+    console.log('Probably listening to heroku $PORT now ', process.env.PORT); // unless $PORT is undefined, in which case you're listening to 8081.
 });
 /*   app.listen(config.port, function () {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
